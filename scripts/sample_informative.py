49126f74ce78c82df8a5775f05165a9ea1edde5e
Nick Walker <nick@nickwalker.us>
2021-03-09 22:21:26 -0600

    Update active batch selection to use MDNs
    Pick dimension to sample trajectories for based on ensemble uncertainty

--- scripts/sample_informative.py
+++ scripts/sample_informative.py
@@ -1,20 +1,25 @@
 import glob
 import itertools
 import json
+import os
 import random
+import sys
 from functools import partial
 
-from joblib import load
+import torch
 import pandas as pd
 
 import numpy as np
 from scipy.spatial.distance import cdist
 
+from models.mdn import ens_uncertainty_kl
+from models.nn import load_ensemble
 from models.util import gen_samples, process_turk_files, load_trajectory_pool, attributions, load_demo_pool
 from search.coverage import trajectory_features
-from search.sampling import sample_neighbors, sample_neighbors_for_pool, sample_random_goal_neighbor
+from search.sampling import sample_perturb_neighbor_pool
 from search.trajectory import TrajectoryNode
 from search.util import load_map
+
 
 random.seed(0)
 num_conditions = 21
@@ -22,51 +27,61 @@ num_conditions = 21
 num_attributions = len(attributions)
 
 
-def iter_uncertainty_sample(model, batch, batch_feats, population, pop_feats, used_indices, balance_lambda):
-    plane_dist = model.decision_function(pop_feats)
-    # Kludge to make sure we don't reuse these
-    total_plane_dist = abs(plane_dist).sum(axis=1)
+def iter_uncertainty_sample(unc, batch, pop_feats, used_indices, balance_lambda):
     if len(batch) == 0:
-      print(total_plane_dist.argmin())
-      return total_plane_dist.argmin(), population[total_plane_dist.argmin()]
-    total_plane_dist[np.array(used_indices)] = float("inf")
+      return unc.argmax()
+    unc[np.array(used_indices)] = float("-inf")
     # Get a matrix of every pairwise similarity
     # Note: cdist is a _distance_, so we'll flip it to similarity (1 means equal, 0 means orthogonal) since that's the usual notation
-    similarity = 1 - cdist(pop_feats, batch_feats, "cosine")
-    # We're gonna want to add the sample that's highly dissimilar to everything in the batch already
-    peak_similarity = similarity.max(axis=1)
-    scores = balance_lambda * total_plane_dist + (1 - balance_lambda) * peak_similarity
-    print(scores.argmin())
-    return scores.argmin(), population[scores.argmin()]
+    similarity = 1 - cdist(pop_feats, pop_feats[np.array(batch)], "cosine")
+    # We care about max similarity to something in the batch already
+    similarity = similarity.max(1)
+    # We're gonna want to add the sample that's highly dissimilar to everything in the batch already (low sim value) and
+    # the model is uncertain about (high unc value). We flip unc sign to make this work.
+    scores = balance_lambda * -unc + (1 - balance_lambda) * similarity
+    return scores.argmin()
 
 
 if __name__ == '__main__':
     grid, bedroom = load_map("interface/assets/map32.tmx")
 
-    condition_ratings, demo_data, other_data = process_turk_files(glob.glob("data/pilot1.csv"))
+    condition_ratings, demo_data, other_data = process_turk_files(["data/pilot1.csv","data/active1.csv", "data/active2.csv", "data/mdn_active1.csv"])
     prompts = list(demo_data["aid"])
     demo_trajs = list(demo_data["traj"])
-
+    print(f"processing {len(demo_trajs)}")
     featurizer = partial(trajectory_features, bedroom, grid)
     TrajectoryNode.featurizer = featurizer
 
-    factor_clfs = load("data/factor_svms.pickle")
+    ens = load_ensemble(os.getcwd() + "/data/ens2")
 
-    neighbors = sample_neighbors_for_pool(demo_trajs, 3, grid)
-    #neighbors = sample_random_goal_neighbor(demo_trajs, 3, grid)
+    # neighbors = sample_neighbors_for_pool(demo_trajs, 3, grid, featurizer)
+    neighbors = sample_perturb_neighbor_pool(demo_trajs, 3, grid, featurizer)
+
     population = demo_trajs + list(itertools.chain.from_iterable(neighbors))
     population_features = list(map(featurizer, population))
+    pool = {"trajectories": str(population), "features": list(map(featurizer, population))}
+    with open("data/total_pool.json", "w") as f:
+        json.dump(pool, f)
+
     origin_per_traj = list(range(len(demo_trajs)))
     for i, neighbors in enumerate(neighbors):
         origin_per_traj = origin_per_traj + [i] * len(neighbors)
 
+    pop_feats = np.array(population_features)
+    unc = ens_uncertainty_kl(ens, torch.Tensor(pop_feats)).detach().numpy()
+
     per_factor = []
     used_indices = []
-    for clf in factor_clfs:
+    # Sort factors by neg mean unc (low value to high value), so we allocate
+    # samples to most uncertain dimension first
+    d_i = np.argsort(-unc.mean(0))
+    print(d_i)
+    for i in d_i:
         per_factor.append([])
-        for _ in range(7):
-            index, sample = iter_uncertainty_sample(clf, per_factor[-1], list(map(featurizer, per_factor[-1])), population, population_features, used_indices, .7)
-            per_factor[-1].append(sample)
+        for _ in range(num_conditions // len(d_i)):
+            # Indexing with list keeps singleton dim
+            index = iter_uncertainty_sample(unc[:,i], per_factor[-1], pop_feats, used_indices, .7)
+            per_factor[-1].append(index)
             used_indices.append(index)
 
     print("Len demos {}, used {}".format(len(demo_trajs), int((pd.DataFrame(used_indices) < len(demo_trajs)).sum())))
