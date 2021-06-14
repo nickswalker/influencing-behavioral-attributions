737c8450d87ef7928ceff71462457a96cbc3fa5b
Nick Walker <nick@nickwalker.us>
2021-06-13 22:07:18 -0700

    Update starting optimal trajectories
    Apply shortcutting to truly trim them down to optimal

--- scripts/generate_attr_trajectory.py
+++ scripts/generate_attr_trajectory.py
@@ -1,35 +1,22 @@
-import glob
-import itertools
-import json
+import os
 import os
 import random
-import sys
 from functools import partial
 
-import scipy
-import torch
-import pandas as pd
-
 import numpy as np
-from joblib import delayed, Parallel
-from scipy.spatial.distance import cdist
-
-from models.mdn.mdn import normal, marginal_mog_log_prob
-from models.mdn.lit_mdn import LitMDN
-from models.mdn.ensemble import MDNEnsemble, ens_uncertainty_kl
-from models.util import gen_samples
-from processing.loading import load_trajectory_pool, load_demo_pool, process_turk_files
-from processing.mappings import attributions
-from search.coverage import trajectory_features, trajectory_cost, CoverageNode, print_feats
-from search.metric import coverage_manhattan
-from search.routine import hill_descend, batch_hill_descend, astar
-from search.sampling import sample_neighbors, sample_neighbors_for_pool
-from search.trajectory import TrajectoryNode, ANY_PLAN
-from search.util import load_map
+import torch
 import torch.nn.functional as F
 import torch.optim.lr_scheduler
-
+from joblib import delayed, Parallel
 from matplotlib import pyplot as plt
+
+from models.mdn.ensemble import MDNEnsemble, ens_uncertainty_kl
+from models.mdn.mdn import normal, marginal_mog_log_prob
+from processing.loading import process_turk_files
+from search.coverage import trajectory_features, trajectory_cost, print_feats
+from search.routine import batch_hill_descend
+from search.trajectory import TrajectoryNode, ANY_PLAN
+from search.util import load_map
 
 random.seed(0)
 np.random.seed(0)
@@ -105,6 +92,20 @@ def sample_attr(inits, d, goal_dist, x, grid, ens, goal_region, max_effort=300, 
     modificiations = batch_hill_descend([TrajectoryNode(orig, None, goal_region=goal_region, cost=trajectory_cost(goal_region, grid, orig)) for orig in inits],
                                         TrajectoryNode(ANY_PLAN, (None,), goal_region=goal_region), grid,
                                         batch_min_div, max_tries=max_effort, tolerance=float("-inf"), branch_limit=250, cost_bound=optimal_cost * cost_threshold, verbose=False)
+    new = modificiations[-1].trajectory
+
+    return new
+
+
+def polish_trajectory(inits, grid, goal_region, max_effort=300):
+    def batch_cost(nodes, goal):
+        if len(nodes) == 0:
+            return np.array([])
+        cost = [node.G for node in nodes]
+        return np.array(cost)
+    modificiations = batch_hill_descend([TrajectoryNode(orig, None, goal_region=goal_region, cost=trajectory_cost(goal_region, grid, orig)) for orig in inits],
+                                        TrajectoryNode(ANY_PLAN, (None,), goal_region=goal_region), grid,
+                                        batch_cost, max_tries=max_effort, tolerance=float("-inf"), branch_limit=250, verbose=False)
     new = modificiations[-1].trajectory
 
     return new
@@ -192,7 +193,7 @@ def main():
     featurizer = partial(trajectory_features, bedroom, grid)
     TrajectoryNode.featurizer = featurizer
 
-    ens = MDNEnsemble.load_ensemble(os.getcwd() + "/data/final")
+    ens = MDNEnsemble.load_ensemble(os.getcwd() + "/data/final_new_feats")
     ens.freeze()
 
     x = np.linspace(-3, 3, 120)
@@ -201,21 +202,15 @@ def main():
     goal_dist = torch.Tensor(normal(1.5, 0.3, x))
     inv_goal_dist = torch.Tensor(normal(-1.5, 0.3, x))
 
-    """
-    opt = astar(CoverageNode(bedroom, (10, 9)), CoverageNode([], (10, 9)), grid, coverage_manhattan)
-    opt =  list(map(lambda x: x.point, opt))
+    """opt = astar(CoverageNode(bedroom, (10, 9)), CoverageNode([], (10, 9)), grid, coverage_manhattan)
+    opt = list(map(lambda x: x.point, opt))
     print("opt", trajectory_cost(bedroom, grid, opt), opt)
-    better_opt = sample_attr([opt], 0, goal_dist, x_b, grid, ens, 10, bedroom)
-    print("better", trajectory_cost(bedroom, grid, better_opt), better_opt)
-    """
-    opt = [(10, 9), (11, 9), (12, 9), (13, 9), (14, 9), (15, 9), (16, 9), (17, 9), (18, 9), (19, 9), (19, 8), (20, 8),
-           (20, 7), (20, 8), (20, 9), (21, 9), (21, 8), (21, 9), (21, 10), (22, 10), (22, 11), (22, 12), (23, 12),
-           (23, 13), (22, 13), (21, 13), (20, 13), (20, 12), (21, 12), (21, 11), (22, 11), (23, 11), (24, 11), (24, 12),
-           (24, 13), (25, 13), (25, 12), (25, 11), (25, 10), (25, 9), (26, 9), (26, 10), (26, 11), (26, 12), (27, 12),
-           (27, 11), (27, 10), (27, 9), (27, 8), (26, 8), (25, 8), (25, 7), (25, 8), (25, 9), (25, 10), (24, 10),
-           (23, 10), (22, 10), (21, 10), (20, 10), (20, 11), (20, 12), (19, 12), (19, 11), (19, 10), (19, 9), (18, 9),
-           (17, 9), (16, 9), (15, 9), (14, 9), (13, 9), (12, 9), (11, 9), (10, 9)]
+    better_opt = polish_trajectory([opt], grid, bedroom)
+    print("better", trajectory_cost(bedroom, grid, better_opt), better_opt)"""
+
+    opt = [(10, 9), (11, 9), (12, 9), (13, 9), (14, 9), (15, 9), (16, 9), (17, 9), (18, 9), (19, 9), (19, 8), (20, 8), (20, 7), (20, 8), (21, 8), (21, 9), (20, 9), (19, 9), (19, 10), (19, 11), (20, 11), (21, 11), (22, 11), (23, 11), (23, 12), (22, 12), (21, 12), (20, 12), (19, 12), (20, 12), (20, 13), (21, 13), (22, 13), (23, 13), (24, 13), (25, 13), (25, 12), (24, 12), (24, 11), (25, 11), (26, 11), (26, 12), (27, 12), (27, 11), (27, 10), (26, 10), (26, 9), (27, 9), (27, 8), (26, 8), (25, 8), (25, 7), (25, 8), (25, 9), (25, 10), (24, 10), (23, 10), (22, 10), (21, 10), (20, 10), (19, 10), (18, 10), (17, 10), (16, 10), (15, 10), (15, 9), (14, 9), (13, 9), (12, 9), (11, 9), (10, 9)]
     opt_cost = trajectory_cost(bedroom, grid, opt)
+    print(opt, opt_cost, opt)
 
     sweep_constraint(opt, goal_dist, 0, grid, bedroom, x_b, ens, opt_cost, "In Competence B+")
     sweep_constraint(opt, inv_goal_dist, 0, grid, bedroom, x_b, ens, opt_cost, "In Competence B-")
@@ -224,28 +219,20 @@ def main():
     sweep_constraint(opt, goal_dist, 2, grid, bedroom, x_b, ens, opt_cost, "In Curious B+")
     sweep_constraint(opt, inv_goal_dist, 2, grid, bedroom, x_b, ens, opt_cost, "In Curious B-")
 
-    print("TSK", trajectory_cost(bedroom, grid, opt))
-
     print("TEST DOMAIN")
     grid, bedroom = load_map("interface/assets/house_test.tmx")
     featurizer = partial(trajectory_features, bedroom, grid)
     TrajectoryNode.featurizer = featurizer
-    """
-    opt = astar(CoverageNode(bedroom, (20,10)), CoverageNode([], (20, 10)), grid, coverage_manhattan)
-    opt =  list(map(lambda x: x.point, opt))
+
+    """opt = astar(CoverageNode(bedroom, (20,10)), CoverageNode([], (20, 10)), grid, coverage_manhattan)
+    opt = list(map(lambda x: x.point, opt))
     print("opt", trajectory_cost(bedroom, grid, opt), opt)
-    better_opt = sample_attr([opt], 0, goal_dist, x_b, grid, ens, 10, bedroom)
-    print("better", trajectory_cost(bedroom, grid, better_opt), better_opt)
-    """
-    opt = [(20, 10), (19, 10), (18, 10), (17, 10), (16, 10), (15, 10), (15, 11), (14, 11), (14, 12), (14, 13), (14, 14),
-           (13, 14), (13, 13), (12, 13), (12, 12), (13, 12), (13, 11), (12, 11), (12, 10), (12, 9), (13, 9), (14, 9),
-           (14, 8), (14, 7), (13, 7), (13, 8), (12, 8), (12, 7), (12, 6), (11, 6), (11, 7), (11, 8), (11, 9), (10, 9),
-           (10, 8), (10, 7), (9, 7), (9, 8), (9, 9), (9, 10), (8, 10), (8, 9), (8, 8), (8, 7), (7, 7), (6, 7), (6, 8),
-           (7, 8), (7, 9), (7, 10), (7, 11), (6, 11), (6, 10), (6, 9), (5, 9), (5, 10), (5, 11), (4, 11), (4, 10),
-           (4, 9), (3, 9), (3, 10), (3, 11), (3, 12), (4, 12), (5, 12), (6, 12), (7, 12), (7, 13), (8, 13), (8, 12),
-           (8, 11), (9, 11), (9, 12), (9, 13), (10, 13), (10, 12), (10, 11), (11, 11), (11, 10), (12, 10), (13, 10),
-           (14, 10), (15, 10), (16, 10), (17, 10), (18, 10), (19, 10), (20, 10)]
+    better_opt = polish_trajectory([opt], grid, bedroom)
+    print("better", trajectory_cost(bedroom, grid, better_opt), better_opt)"""
+
+    opt = [(20, 10), (19, 10), (18, 10), (17, 10), (16, 10), (15, 10), (14, 10), (14, 11), (14, 12), (14, 13), (14, 14), (13, 14), (13, 13), (12, 13), (12, 12), (13, 12), (13, 11), (12, 11), (12, 10), (12, 9), (13, 9), (14, 9), (14, 8), (14, 7), (13, 7), (13, 8), (12, 8), (12, 7), (12, 6), (13, 6), (12, 6), (11, 6), (11, 7), (11, 8), (11, 9), (10, 9), (10, 8), (10, 7), (9, 7), (9, 6), (9, 7), (9, 8), (9, 9), (9, 10), (8, 10), (8, 9), (8, 8), (8, 7), (7, 7), (6, 7), (6, 8), (7, 8), (7, 9), (7, 10), (7, 11), (6, 11), (6, 10), (6, 9), (5, 9), (5, 10), (5, 11), (4, 11), (4, 10), (4, 9), (3, 9), (3, 10), (3, 11), (3, 12), (4, 12), (5, 12), (6, 12), (7, 12), (7, 13), (8, 13), (8, 12), (8, 11), (9, 11), (9, 12), (9, 13), (9, 14), (9, 13), (10, 13), (10, 12), (10, 11), (11, 11), (11, 10), (10, 10), (11, 10), (12, 10), (13, 10), (14, 10), (15, 10), (16, 10), (17, 10), (18, 10), (19, 10), (20, 10)]
     opt_cost = trajectory_cost(bedroom, grid, opt)
+    print(opt, opt_cost, opt)
 
     sweep_constraint(opt, goal_dist, 0, grid, bedroom, x_b, ens, opt_cost, "Test Competence B+")
     sweep_constraint(opt, inv_goal_dist, 0, grid, bedroom, x_b, ens, opt_cost, "Test Competence B-")
